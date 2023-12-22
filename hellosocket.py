e237cd8aa0b9f1efb6197f3f7acccfdaa5bb58c0
Bomfunk <mrbomfunk@gmail.com>
2023-12-22 12:47:34 +0100

    Fixed PEP8 compliancy

--- hellosocket.py
+++ hellosocket.py
@@ -1,6 +1,7 @@
 import socket
 import datetime
 
+
 def start_server(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', port))
@@ -13,10 +14,12 @@ def start_server(port):
 
         print(f"Connection from {client_address}")
 
-        message = datetime.datetime.now().strftime("Hello world! %Y-%m-%d %H:%M:%S\n")
+        message = datetime.datetime.now().\
+            strftime("Hello world! %Y-%m-%d %H:%M:%S\n")
         client_socket.send(message.encode("utf-8"))
 
         client_socket.close()
+
 
 if __name__ == "__main__":
     port_number = 12345
